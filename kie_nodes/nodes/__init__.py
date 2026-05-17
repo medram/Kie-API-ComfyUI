5481bc6b620dfebaad22d14d301f4b39166e4893
medram <mramouchy@gmail.com>
2026-05-17 23:32:26 +0100

    📦 Register all new nodes in package exports

--- kie_nodes/nodes/__init__.py
+++ kie_nodes/nodes/__init__.py
@@ -3,13 +3,26 @@ from .kie_gpt_image_2_node import KieGptImage2Node
 from .kie_grok_imagine_i2v_node import KieGrokImagineI2VNode
 from .kie_grok_imagine_t2i_node import KieGrokImagineT2INode
 from .kie_grok_imagine_t2v_node import KieGrokImagineT2VNode
+from .kie_happyhorse_node import KieHappyHorseNode
+from .kie_happyhorse_r2v_node import KieHappyHorseR2VNode
+from .kie_happyhorse_video_edit_node import KieHappyHorseVideoEditNode
 from .kie_image_loader_node import KieImageLoaderNode
 from .kie_image_preview_node import KieImagePreviewNode
+from .kie_kling_2_6_video_node import KieKling26VideoNode
+from .kie_kling_3_0_video_node import KieKling30VideoNode
+from .kie_kling_ai_avatar_node import KieKlingAiAvatarNode
 from .kie_nanobanana_2_node import KieNanoBanana2Node
 from .kie_nanobanana_pro_node import KieNanoBananaProNode
+from .kie_qwen2_image_edit_node import KieQwen2ImageEditNode
+from .kie_seedance_1_5_pro_node import KieSeedance15ProNode
 from .kie_seedance_2_node import KieSeedance2Node
 from .kie_seedream_4_5_node import KieSeedream45Node
+from .kie_suno_node import KieSunoNode
 from .kie_text_node import TextNode
+from .kie_veo3_node import KieVeo3Node
+from .kie_wan_22_animate_node import KieWan22AnimateNode
+from .kie_wan_26_node import KieWan26Node
+from .kie_wan_27_node import KieWan27Node
 from .kie_z_image_node import KieZImageNode
 
 __all__ = [
@@ -17,13 +30,26 @@ __all__ = [
     "KieGptImage2Node",
     "KieGrokImagineI2VNode",
     "KieGrokImagineT2INode",
+    "KieHappyHorseNode",
+    "KieHappyHorseR2VNode",
+    "KieHappyHorseVideoEditNode",
     "KieGrokImagineT2VNode",
     "KieImageLoaderNode",
     "KieImagePreviewNode",
+    "KieKling26VideoNode",
+    "KieKling30VideoNode",
+    "KieKlingAiAvatarNode",
     "KieNanoBanana2Node",
     "KieNanoBananaProNode",
+    "KieSeedance15ProNode",
     "KieSeedance2Node",
+    "KieQwen2ImageEditNode",
     "KieSeedream45Node",
+    "KieVeo3Node",
+    "KieWan22AnimateNode",
+    "KieWan26Node",
+    "KieWan27Node",
+    "KieSunoNode",
     "KieZImageNode",
     "TextNode",
 ]
